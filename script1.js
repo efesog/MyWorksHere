18556e7f0d352689071c4994ae210c3f35704019
efesog <eltkoko@yahoo.com>
2022-11-03 13:47:54 +0300

    third

---
+++ script1.js
@@ -0,0 +1,9 @@
+var i = 6.232, j= 43.534, k=17.4999999 
+    document.write(i + " sayısını yuvarlarsak: " + Math.round(i) + "<br>") 
+    document.write(j + " sayısını yuvarlarsak: " + Math.round(j) + "<br>") 
+    document.write(k + " sayısını yuvarlarsak: " + Math.round(k) + "<br>") 
+    document.write( Math.sqrt(i) + "<br>")
+    document.write(Math.floor(k)+ "<br>")
+    document.write(Math.ceil(k))
+    alert ("Hello CANANOY")
+
